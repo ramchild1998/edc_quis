cae3664c96711bb44bdbdc94197a70e2d5c4e5a0
ramchild1998 <ramadiansyah1998@gmail.com>
2024-09-27 14:19:59 +0700

    Role and Permissions Plugin Installed

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,10 +6,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use SolutionForest\FilamentAccessManagement\Concerns\FilamentUserHelpers;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, FilamentUserHelpers;
 
     /**
      * The attributes that are mass assignable.
