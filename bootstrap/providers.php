7ea67029864254b97183bb701898fae0e6e44d3a
Ikhlasul Kamal <ikhlasul.kamal.i@gmail.com>
2024-09-30 17:06:25 +0700

    adding initial teknisi panel

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,6 +3,7 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\Filament\AdminPanelProvider::class,
+    App\Providers\Filament\TeknisiPanelProvider::class,
     App\Providers\TelescopeServiceProvider::class,
     Spatie\Permission\PermissionServiceProvider::class,
 ];
