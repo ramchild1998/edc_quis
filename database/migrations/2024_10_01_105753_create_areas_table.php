5e0bb18bcf05d4d5875d56045366affbcc2ea754
Ikhlasul Kamal <ikhlasul.kamal.i@gmail.com>
2024-10-01 11:12:08 +0700

    adding area and maping_area

---
+++ database/migrations/2024_10_01_105753_create_areas_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('area', function (Blueprint $table) {
+            $table->integerIncrements('id')->primary();
+            $table->string('area_name', 45);
+            $table->boolean('status');
+            $table->unsignedBigInteger('created_by');
+            $table->foreign('created_by')->references('id')->on('users');
+            $table->unsignedBigInteger('updated_by');
+            $table->foreign('updated_by')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('area');
+    }
+};
