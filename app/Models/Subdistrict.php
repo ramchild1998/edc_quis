186c66fd3de39a28568630a5d5564c6a1467ae2c
Ikhlasul Kamal <ikhlasul.kamal.i@gmail.com>
2024-10-01 09:52:43 +0700

    adding initial migration and model for locations

---
+++ app/Models/Subdistrict.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Subdistrict extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+            'subdistrict_name',
+            'district_id',
+        ];
+
+    public $timestamps = false;
+}
