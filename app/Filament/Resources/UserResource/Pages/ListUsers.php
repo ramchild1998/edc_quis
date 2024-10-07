7add594ae50650b7a1dd899e71a5f4cd80300284
ramchild1998 <ramadiansyah1998@gmail.com>
2024-10-07 16:53:49 +0700

    User Export -> ListUsers

--- app/Filament/Resources/UserResource/Pages/ListUsers.php
+++ app/Filament/Resources/UserResource/Pages/ListUsers.php
@@ -5,6 +5,9 @@ namespace App\Filament\Resources\UserResource\Pages;
 use App\Filament\Resources\UserResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use pxlrbt\FilamentExcel\Columns\Column;
+use pxlrbt\FilamentExcel\Exports\ExcelExport;
+use pxlrbt\FilamentExcel\Actions\Pages\ExportAction;
 
 class ListUsers extends ListRecords
 {
@@ -14,6 +17,16 @@ class ListUsers extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+            ExportAction::make()
+            ->exports([
+                ExcelExport::make()
+                    ->fromTable()
+                    ->withFilename(fn ($resource) => $resource::getModelLabel() . '-' . date('Y-m-d'))
+                    ->withWriterType(\Maatwebsite\Excel\Excel::XLSX)
+                    ->withColumns([
+                        Column::make('updated_at'),
+                    ])
+            ]),
         ];
     }
 }
