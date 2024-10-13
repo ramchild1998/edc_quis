45fbfcf60832b052676ac294cca9e57c522f61ab
Ikhlasul Kamal <ikhlasul.kamal.i@gmail.com>
2024-10-13 11:17:14 +0700

    adding visit migration

---
+++ database/migrations/2024_10_13_100409_create_visit.php
@@ -0,0 +1,92 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('visit', function (Blueprint $table) {
+            // Area Category
+            $table->integerIncrements('id');
+            $table->char('visit_id', 10);
+            $table->unsignedInteger('vendor_id');
+            $table->unsignedInteger('area_id');
+            $table->unsignedInteger('location_id');
+            $table->string('nama_lokasi', 100)->nullable();
+            $table->string('keterangan_lokasi', 22)->nullable();
+            $table->string('nama_usaha', 100);
+            $table->string('alamat_usaha');
+            $table->unsignedInteger('province_id');
+            $table->unsignedInteger('city_id');
+            $table->unsignedInteger('district_id');
+            $table->unsignedInteger('subdistrict_id');
+            $table->unsignedInteger('poscode_id');
+
+            // Merchant Infromation Category
+            $table->char('mid', 9)->nullable();
+            $table->text('tid')->nullable();
+            $table->char('nomor_sn', 24)->nullable();
+            $table->string('nama_pemilik', 45)->nullable(); 
+            $table->string('no_kontak', 20)->nullable();
+            $table->boolean('alamat_edc_sesuai')->nullable();
+            $table->boolean('ada_edc_bca')->nullable();
+            $table->unsignedTinyInteger('jumlah_edc')->nullable();
+            $table->boolean('edc_bank_lain')->nullable();
+            $table->text('list_edc_bank_lain')->nullable();
+            $table->string('catatan_kunjungan_edc', 22)->nullable();
+            $table->boolean('qris_bca')->nullable();
+            $table->string('nmid', 15)->nullable();
+            $table->text('list_qris_bank_lain')->nullable();
+            $table->boolean('tes_transaksi')->nullable();
+            $table->string('catatan_kunjungan_program', 50)->nullable();
+            $table->text('kendala')->nullable();
+            $table->text('request')->nullable();
+            $table->unsignedTinyInteger('jumlah_struk')->nullable();
+            $table->string('id_lapor_halo', 8)->nullable();
+
+            // Potential Merchant Category
+            $table->string('pengajuan_merchant', 15)->nullable();
+            $table->string('aplikasi_pendaftaran', 25)->nullable();
+            $table->string('fdm_id', 7)->nullable();
+            $table->string('alasan_tidak_bersedia', 50)->nullable();
+            $table->boolean('mempunyai_edc')->nullable();
+            $table->string('keterangan_lain', 50)->nullable();
+            $table->string('nomor_referensi')->nullable();
+
+            // Bukti Kunjungan
+            $table->string('foto_struk')->nullable();
+            $table->string('foto_tampak_depan')->nullable();
+            $table->string('foto_meja_kasir')->nullable();
+            $table->string('foto_qris_statis')->nullable();
+            $table->string('foto_selfie')->nullable();
+            $table->string('foto_produk')->nullable();
+            $table->string('screen_capture')->nullable();
+            $table->date('tanggal_submit');
+            $table->time('waktu_submit');
+            $table->string('nama_surveyor');
+            $table->string('upline1')->nullable();
+            $table->string('sales_code', 15)->nullable();
+
+            // Tambahan
+            $table->decimal('lat', 10, 8)->nullable();
+            $table->decimal('long', 11, 8)->nullable();
+            $table->string('created_by');
+            $table->string('updated_by');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('visit');
+    }
+};
