61f730ca4f0fac3233662e6e207fd25fcca2356a
ramchild1998 <ramadiansyah1998@gmail.com>
2024-10-13 11:39:27 +0700

    Visit Mobile Update

--- app/Models/Visit.php
+++ app/Models/Visit.php
@@ -10,89 +10,73 @@ class Visit extends Model
 {
     use HasFactory;
 
-    protected $table = 'kunjungan';
+    protected $table = 'visit';
 
     protected $fillable = [
-        'order_id',
-        'nama_usaha',
-        'key_search',
-        'alamat_usaha',
-        'keterangan_lokasi',
+        'id_visit',
+        'vendor_id',
+        'area_id',
+        'location_id',
         'nama_lokasi',
-        'alamat_edc_sesuai',
-        'mall',
-        'apakah_ada_edc',
-        'jumlah_edc',
+        'keterangan_lokasi',
+        'nama_usaha',
+        'alamat_usaha',
+        'province_id',
+        'city_id',
+        'district_id',
+        'subdistrict_id',
+        'poscode_id',
         'mid',
         'tid',
+        'nomor_sn',
         'nama_pemilik',
-        'nomor_kontak_pemilik',
+        'no_kontak',
+        'alamat_edc_sesuai',
+        'ada_edc_bca',
+        'jumlah_edc',
         'edc_bank_lain',
-        'daftar_edc_bank_lain',
-        'qris',
-        'qris_bank',
-        'cat_edukasi_penggunaan_edc',
-        'cat_sosialisasi_aplikasi',
-        'cat_informasi_brand',
-        'cat_tes_transaksi',
-        'ken_jaringan',
-        'ken_baterai',
-        'ken_adaptor',
-        'ken_tombol',
-        'ken_others',
-        'ken_tidak_ada_kendala',
-        'ken_hardware',
-        'ken_komunikasi',
-        'ken_lainnya',
-        'jumlah_kertas',
+        'list_edc_bank_lain',
+        'catatan_kunjungan_edc',
+        'qris_bca',
+        'nmid',
+        'list_qris_bank_lain',
+        'tes_transaksi',
+        'catatan_kunjungan_program',
+        'kendala',
+        'request',
+        'jumlah_struk',
         'id_lapor_halo',
-        'request_tambahan_fasilitas',
-        'request_ganti_ke_apos',
-        'request_tambah_cabang',
-        'request_tambah_terminal',
-        'request_permintaan_struk',
-        'request_others',
-        'pengajuan_existing',
-        'pengajuan_merchant_bca',
+        'pengajuan_merchant',
         'aplikasi_pendaftaran',
         'fdm_id',
         'alasan_tidak_bersedia',
-        'mempunyai_edc_bca',
+        'mempunyai_edc',
         'keterangan_lain',
-        'foto_struk_transaksi',
+        'nomor_referensi',
+        'foto_struk',
         'foto_tampak_depan',
         'foto_meja_kasir',
         'foto_qris_statis',
-        'foto_selfie_dengan_pemilik',
+        'foto_selfie',
         'foto_produk',
         'screen_capture',
         'tanggal_submit',
-        'time_submit',
+        'waktu_submit',
         'nama_surveyor',
-        'username',
         'upline1',
-        'upline2',
-        'upline3',
-        'kota',
-        'nip',
         'sales_code',
-        'verifikasi_mid',
-        'nomor_referensi',
-        'latitude',
-        'longitude',
-        'status',
-        'area_id',
-        'maping_area_id',
+        'lat',
+        'long',
         'created_by',
         'updated_by',
-        // 'province_id',
-        // 'city_id',
-        // 'district_id',
-        // 'subdistrict_id',
-        // 'poscode_id',
     ];
 
     public $timestamps = true;
+
+    public function vendor(): BelongsTo
+    {
+        return $this->belongsTo(Vendor::class);
+    }
 
     public function area()
     {
@@ -104,30 +88,30 @@ class Visit extends Model
         return $this->belongsTo(MapingArea::class); // Pastikan ini sesuai dengan relasi yang Anda inginkan
     }
 
-    // public function province()
-    // {
-    //     return $this->belongsTo(Province::class); // Pastikan ini sesuai dengan relasi yang Anda inginkan
-    // }
+    public function province()
+    {
+        return $this->belongsTo(Province::class); // Pastikan ini sesuai dengan relasi yang Anda inginkan
+    }
 
-    // public function city()
-    // {
-    //     return $this->belongsTo(City::class);
-    // }
+    public function city()
+    {
+        return $this->belongsTo(City::class);
+    }
 
-    // public function district()
-    // {
-    //     return $this->belongsTo(District::class);
-    // }
+    public function district()
+    {
+        return $this->belongsTo(District::class);
+    }
 
-    // public function subdistrict()
-    // {
-    //     return $this->belongsTo(Subdistrict::class);
-    // }
+    public function subdistrict()
+    {
+        return $this->belongsTo(Subdistrict::class);
+    }
 
-    // public function poscode()
-    // {
-    //     return $this->belongsTo(Poscode::class);
-    // }
+    public function poscode()
+    {
+        return $this->belongsTo(Poscode::class);
+    }
 
     public function createdBy(): BelongsTo
     {
